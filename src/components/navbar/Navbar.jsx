498f8b48105125e86d27cfaf69a001ecb1d8fc0b
Fabio Migliaccio <febbio9@gmail.com>
2023-02-22 17:07:25 +0100

    top navbar branch create

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Container, Nav, Navbar } from 'react-bootstrap'
+import logo from '../../assets/logo.png'
 
-export const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+
+
+export default function Navbar() {
+    return (
+        <Navbar >
+            <Container>
+                <Navbar.Brand href="#home">
+                    <img src={logo} alt=""/>
+                    
+                    <Navbar.Text > <Link className="text-decoration-none  text-white"  to={'/'}>Soundwave</Link> </Navbar.Text>
+                </Navbar.Brand>
+
+                
+                    <Nav className="ms-auto">
+                        <Nav.Link className="text-white text-xl" ><Link  className="text-decoration-none text-white  " to={'/Discover'}>Discover</Link></Nav.Link>
+                        <Nav.Link ><Link className="text-decoration-none text-white " to={'/Join'}>Join</Link></Nav.Link>
+                    </Nav>
+                
+            </Container>
+        </Navbar>
+
+    )
 }
