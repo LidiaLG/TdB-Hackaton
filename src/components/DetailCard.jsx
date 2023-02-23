de3a8d8655cfd7999cbf5edd4478684a92ac8163
LidiaLG <lidialuquegallego91@gmail.com>
2023-02-23 14:12:34 +0100

    finished detail product

---
+++ src/components/DetailCard.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import { MDBIcon } from 'mdb-react-ui-kit';
+
+
+const DetailCard = (props) => {
+    return (
+        <>
+            <Card className="p-1 m-4 mb-10" style={{ maxWidth: "30rem", height: "100%"}}>
+                <Card.Img src={props.img} />
+                <Card.Body>
+                    <Card.Title style={{fontSize: '25px'}}>{props.title}</Card.Title>
+                    <Card.Text>{props.descripcion}</Card.Text>
+                    <Card.Text style={{fontSize: '20px'}}>{props.precio}</Card.Text>
+                    <Card.Body className="d-flex justify-content-around">
+                        <Button className="rounded-8" style={{backgroundColor: '#C06500'}}>Comprar</Button>
+                        <MDBIcon fas icon="heart" role='button' style={{color: '#C06500', fontSize: '40px'}}/>
+                    </Card.Body>
+                </Card.Body>
+            </Card>
+        </>
+    )
+}
+
+export default DetailCard
--- src/components/views/Product.jsx
+++ src/components/views/Product.jsx
@@ -1,15 +1,24 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import DetailCard from "../DetailCard";
+import Footer from "../footer/Footer";
+import NavBar from "../navbar/Navbar";
 
 
 export default function Product() {
     return (
       <>
+        <NavBar/>
+        <Container className="d-flex justify-content-center">
+        <DetailCard
+          img="https://www.shbarcelona.es/blog/es/wp-content/uploads/2015/06/clases-guitarra-ensenada-mexico.jpg"
+          title="Clases de guitarra"
+          descripcion="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived"
+          precio="15 €"
+        />
+        </Container>
         
-
-        <h1>Product</h1>
-        
-        
-      
+        <Footer/>
       </>
     );
   }
