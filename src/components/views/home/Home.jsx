fc2b02615d8f4bb183ee7570f28a16dca8ced0d2
Denis Gladarenko <gladarenko.denis@gmail.com>
2023-02-23 13:53:14 +0100

    crate Container Home

--- src/components/views/home/Home.jsx
+++ src/components/views/home/Home.jsx
@@ -3,39 +3,53 @@ import Footer from "../../footer/Footer";
 import NavbarTop from "../../navbar/Navbar";
 import CardPrincipal from "./CardPrincipal";
 import styles from './Home.module.css'
+import { Container, Row, Col } from "react-bootstrap";
 
 const Home = () => {
   return (
     <div>
+        
+        <NavbarTop/>
 
-<NavbarTop/>
-<h1 className={styles.h1}> Tu tienda de barrio al ancance de tu mano</h1>
+        <Container style={{paddingTop: '2rem', paddingBottom: '2rem'}}>
+          <Row>
+            
+             <h1 className={styles.h1}> Tu tienda de barrio al ancance de tu mano</h1>
+           <Col>
+            <CardPrincipal
+                img="https://www.shbarcelona.es/blog/es/wp-content/uploads/2015/06/clases-guitarra-ensenada-mexico.jpg"
+                title="Clases de guitarra"
+                precio="15 €"
+              /> 
+          </Col>
+          <Col>
+            <CardPrincipal
+              img="https://www.shbarcelona.es/blog/es/wp-content/uploads/2015/06/clases-guitarra-ensenada-mexico.jpg"
+              title="Clases de guitarra"
+              precio="15 €"/>
+          </Col>
+          <Col>
+            <CardPrincipal
+              img="https://www.shbarcelona.es/blog/es/wp-content/uploads/2015/06/clases-guitarra-ensenada-mexico.jpg"
+              title="Clases de guitarra"
+              precio="15 €"/>
+          </Col>
+          <Col>
+            <CardPrincipal
+              img="https://www.shbarcelona.es/blog/es/wp-content/uploads/2015/06/clases-guitarra-ensenada-mexico.jpg"
+              title="Clases de guitarra"
+              precio="15 €"/>
+          </Col>
+          <Col>
+            <CardPrincipal
+              img="https://www.shbarcelona.es/blog/es/wp-content/uploads/2015/06/clases-guitarra-ensenada-mexico.jpg"
+              title="Clases de guitarra"
+              precio="15 €"/>
+          </Col>
+          </Row>    
+        </Container>
 
-      <CardPrincipal
-        img="https://www.shbarcelona.es/blog/es/wp-content/uploads/2015/06/clases-guitarra-ensenada-mexico.jpg"
-        title="Clases de guitarra"
-        precio="15 €"
-      />
-      <CardPrincipal
-      img="https://www.shbarcelona.es/blog/es/wp-content/uploads/2015/06/clases-guitarra-ensenada-mexico.jpg"
-      title="Clases de guitarra"
-      precio="15 €"/>
-
-<CardPrincipal
-      img="https://www.shbarcelona.es/blog/es/wp-content/uploads/2015/06/clases-guitarra-ensenada-mexico.jpg"
-      title="Clases de guitarra"
-      precio="15 €"/>
-
-<CardPrincipal
-      img="https://www.shbarcelona.es/blog/es/wp-content/uploads/2015/06/clases-guitarra-ensenada-mexico.jpg"
-      title="Clases de guitarra"
-      precio="15 €"/>
-
-<CardPrincipal
-      img="https://www.shbarcelona.es/blog/es/wp-content/uploads/2015/06/clases-guitarra-ensenada-mexico.jpg"
-      title="Clases de guitarra"
-      precio="15 €"/>
-      <Footer/> 
+              <Footer/> 
     </div>
   );
 };
